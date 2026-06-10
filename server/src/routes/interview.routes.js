94d3413a0a9435c2ab2f60995543e7b6f4e49d8b
Samarth Pagaria <pagariasamarth@gmail.com>
2026-06-10 23:26:48 +0530

    feat_routes : interview routes add

---
+++ server/src/routes/interview.routes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { authMiddleware } from "../middlewares/auth.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { getInterviewReportController, generateResumePdfController } from "../controllers/interview.controller.js";
+
+const router = express.Router();
+
+router.use(authMiddleware);
+/**
+ * @route POST /api/interview/report
+ * @desc Generates an interview report based on resume, job description, and self-description.
+ * @access Private
+ */
+router.post("/", upload.single("resume"), getInterviewReportController);
+/**
+ * @route POST /api/interview/report/:interviewReportId/pdf
+ * @desc Generates a PDF of the interview report.
+ * @access Private
+ */
+router.post("/resume/pdf/:interviewReportId", generateResumePdfController);
+
+export default router;
